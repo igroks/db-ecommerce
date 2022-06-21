868e161ef715146e0f22dafaba82a290dc2814f8
Igor <igor.carvalho@icomp.ufam.edu.br>
2022-06-21 00:48:40 -0400

    feat: add insertion to database and fix the schema

--- src/tp1_3.2.py
+++ src/tp1_3.2.py
@@ -1,7 +1,6 @@
 import re
 import os
 import psycopg2
-from psycopg2 import Error
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -14,131 +13,143 @@ POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
 
 lineContentRegex = re.compile(r'^(?:)?([A-Za-z]+):\s*(.+)$')
 reviewsContentRegex = re.compile(r'^([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})\s+cutomer:\s+([A-Z0-9]+?)\s+rating:\s+([1-5])\s+votes:\s+([0-9]+?)\s+helpful:\s+([0-9]+)$')
+schema = [ 
+    '''
+    CREATE TABLE IF NOT EXISTS Product( 
+        asin TEXT PRIMARY KEY,
+        title TEXT,
+        product_group TEXT,
+        salesrank INTEGER,
+        similars INTEGER
+    )
+    ''',
+    '''
+    CREATE TABLE IF NOT EXISTS Similars(
+        s_asin TEXT,
+        asin_similars TEXT,
+        PRIMARY KEY(s_asin, asin_similars),
+        FOREIGN KEY(s_asin) REFERENCES Product(asin)
+    )
+    ''',
+    '''
+    CREATE TABLE IF NOT EXISTS Reviews_catolog(
+        cat_asin TEXT,
+        total INTEGER,
+        downloaded INTEGER,
+        avg_rating INTEGER,
+        PRIMARY KEY(cat_asin),
+    )
+    ''',
+    '''
+    CREATE TABLE IF NOT EXISTS Reviews(
+        comment_asin TEXT, 
+        id_client TEXT,
+        date DATE,
+        rating INTEGER,
+        votes INTEGER,
+        helpful INTEGER,
+        PRIMARY KEY (comment_asin, id_client),
+        FOREIGN KEY(comment_asin) REFERENCES Product(asin)
+    )
+    ''',
+    '''        
+    CREATE TABLE IF NOT EXISTS Category(
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+    ''',
+    '''
+    CREATE TABLE IF NOT EXISTS Products_per_category(
+        ppc_asin TEXT PRIMARY KEY, 
+        cat_id INTEGER
+    )
+    '''
+]
+
+attributeMap = {
+    'Product':['ASIN','title', 'group', 'salesrank', 'num_similars']
+}
 
 def formatLine(line):
     return re.sub(r'\s{2,}?', ' ', line).replace('\n','').strip()
 
 def readDatasFromFile():
     products = []
+    lines = None
+    attr = ''
+    product = {}
    
     with open(INPUT_FILE) as f:
         lines = f.readlines()
-        attr = ''
-        product = {}
 
-        for line in lines:
-            line = formatLine(line)
+    for line in lines:
+        line = formatLine(line)
 
-            if not line and product:
-                products.append(product)
-                product = {}
+        if not line and product:
+            products.append(product)
+            product = {}
 
-            contentMatch = lineContentRegex.match(line)
+        contentMatch = lineContentRegex.match(line)
 
-            if contentMatch and len(contentMatch.groups()) == 2:
-                attr, value = contentMatch.groups()
-                if attr == 'similar':
-                    product[attr] = value.split('  ')[1:]
-                elif attr in ['salesrank','Id']:
-                    product[attr] = int(value)
-                elif attr in ['categories', 'reviews']:
-                    product[attr] = []
-                else:
-                    product[attr] = value
-            elif attr == 'categories':
-                product[attr] = list(set(product[attr] + line.split('|')[1:]))
-            elif attr == 'reviews':
-                reviewsMatch = reviewsContentRegex.match(line)
-                if reviewsMatch:
-                    year, month, day = reviewsMatch.group(1,2,3)
-                    product[attr].append(
-                        {
-                            'date': f'{year}-{month.rjust(2,"0")}-{day.rjust(2,"0")}',
-                            'customer': reviewsMatch.group(4),
-                            'rating': int(reviewsMatch.group(5)),
-                            'votes': int(reviewsMatch.group(6)),
-                            'helpful': int(reviewsMatch.group(7))
-                        }
-                    )
+        if contentMatch and len(contentMatch.groups()) == 2:
+            attr, value = contentMatch.groups()
+            if attr == 'similar':
+                value = value.split(' ')
+                product['num_similars'] = value[0]
+                product[attr] = value[1:]
+            else:
+                product[attr] = value if not attr in ['categories','reviews'] else []
+        elif attr == 'categories':
+            product[attr] = list(set(product[attr] + line.split('|')[1:]))
+        elif attr == 'reviews':
+            reviewsMatch = reviewsContentRegex.match(line)
+            if reviewsMatch:
+                year, month, day = reviewsMatch.group(1,2,3)
+                product[attr].append({
+                    'date': f'{year}-{month.rjust(2,"0")}-{day.rjust(2,"0")}',
+                    'customer': reviewsMatch.group(4),
+                    'rating': reviewsMatch.group(5),
+                    'votes': reviewsMatch.group(6),
+                    'helpful': reviewsMatch.group(7)
+                })
+    return products
 
 def connectDataBase():
     return psycopg2.connect(
         host=POSTGRES_HOST,
         user=POSTGRES_USER,
         password=POSTGRES_PASSWORD,
-        database=POSTGRES_DATABASE
+        database=POSTGRES_DATABASE,
+        port=POSTGRES_DOCKER_PORT
     )
 
-def createTables():
-    commands = [ 
-        '''
-        CREATE TABLE Product( 
-            asin INTEGER PRIMARY KEY,
-            title TEXT,
-            product_group TEXT,
-            salesrank INTEGER,
-            similars INTEGER
-        )
-        ''',
-        '''
-        CREATE TABLE Comments_catalog(
-            cat_asin INTEGER PRIMARY KEY,
-            total INTEGER,
-            downloaded INTEGER,
-            avg_rating INTEGER,
-            FOREIGN KEY(cat_asin) REFERENCES Product(asin)
-        )
-        ''',
-        '''
-        CREATE TABLE Comments(
-            comment_asin INTEGER, 
-            id_client INTEGER,
-            date DATE,
-            rating INTEGER,
-            votes INTEGER,
-            helpful INTEGER,
-            PRIMARY KEY (comment_asin, id_client),
-            FOREIGN KEY(comment_asin) REFERENCES Product(asin)
-        )
-        ''',
-        '''
-        CREATE TABLE Similars(
-            s_asin INTEGER,
-            asin_similars INTEGER,
-            PRIMARY KEY(s_asin, asin_similars),
-            FOREIGN KEY(s_asin) REFERENCES Product(asin)
-        )
-        ''',
-        '''        
-        CREATE TABLE Category(
-            id INTEGER PRIMARY KEY,
-            name TEXT
-        )
-        ''',
-        '''
-        CREATE TABLE Products_per_category(
-            ppc_asin INTEGER PRIMARY KEY, 
-            cat_id INTEGER
-        )
-        '''
-    ] 
+def executeCommand(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
 
+def closeDataBase(connection):
+    if (connection):
+        connection.close()
+        print('PostgreSQL connection is closed')
+
+def buidInsertQuery(table, args):
+    return f'INSERT INTO {table} VALUES {tuple(args)}'.replace('None','NULL')
+
+if __name__ == '__main__':
+    connection = None
     try:
         connection = connectDataBase()
-        cursor = connection.cursor()
-        
-        for command in commands:
-            cursor.execute(command)
-        
-        connection.commit()
-    except (Exception, Error) as error:
+        for t in schema:
+            executeCommand(connection, t)
+
+        for product in readDatasFromFile():
+            executeCommand (
+                connection, 
+                buidInsertQuery('Product', [product.get(arg) for arg in attributeMap['Product']]),
+            )
+    except (Exception, psycopg2.Error) as error:
         print('Error while connecting to PostgreSQL', error)
     finally:
-        if (connection):
-            cursor.close()
-            connection.close()
-            print('PostgreSQL connection is closed')
-   
-if __name__ == '__main__':
-    readDatasFromFile()
-    createTables()
+        closeDataBase(connection) 

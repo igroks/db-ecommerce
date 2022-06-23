184f4e15243175b86f722b47ec540784bf2c2b64
erlonbie <erlonminievix@gmail.com>
2022-06-23 12:35:54 -0400

    feat: add queryString for any class

--- src/tp1_3.2.py
+++ src/tp1_3.2.py
@@ -26,11 +26,14 @@ reviewsContent = re.compile(
 class Product(object):
     """Product class -> (asin, title, productGroup, salesrank)"""
 
-    def __init__(self, asin, title=None, productGroup=None, salesrank=None):
+    def __init__(self, asin, title=None, product_group=None, salesrank=None):
         self.asin = asin
         self.title = title
-        self.productGroup = productGroup
+        self.product_group = product_group
         self.salesrank = salesrank
+
+    def __repr__(self) -> str:
+        return f'{self.asin}, {self.title}, {self.product_group}, {self.salesrank}'
 
 
 class CommentsCatalog(object):
@@ -80,6 +83,16 @@ class Products_Per_Category(object):
         self.catId = catId
 
 
+# def createClass(className, *args):
+#     return className(*args)
+
+
+# def addDatabase(*args):
+#     for dataTable in args:
+#         dataTableDict = createClass(dataTable[0], dataTable[1:])
+#         for item in dataTable:
+
+
 def parseData():
 
     products = []
@@ -87,15 +100,12 @@ def parseData():
         lines = f.readlines()
         inBlock = ''
         product = {}
-        product['product'] = ()
-        categoriesSet = set()
 
         for line in lines:
             line = re.sub(r'\s{2,}?', ' ', line).replace('\n', '').strip()
             if not line and product:
                 products.append(product)
                 product = {}
-                product['product'] = ()
 
             m = lineContent.match(line)
 
@@ -108,14 +118,15 @@ def parseData():
                 elif m.group(1) == 'reviews':
                     inBlock = 'reviews'
                 else:
-                    product['product'] += (m.group(2),)
+                    if m.group(1) == 'ASIN' or m.group(1) == 'salesrank':
+                        product[m.group(1)] = int(m.group(2))
+                    else:
+                        product[m.group(1)] = m.group(2)
             else:
                 if inBlock == 'categories':
                     categoriesSet = set()
                     lineCategory = line.split('|')[1:]
                     for i in range(len(lineCategory)):
-                        # individualCategory = re.search(
-                        #     r"(.*)\[(\d+)\]", lineCategory[i])
                         if i > 0:
                             fatherCategory = re.search(
                                 r"(.*)\[(\d+)\]", lineCategory[i-1])
@@ -129,27 +140,60 @@ def parseData():
                             categoriesSet.add((fatherCategory.group(
                                 1), fatherCategory.group(2), None))
 
-                    # product['categories'] = list(
-                    #     set(product['categories'] + line.split('|')[1:]))
                     product['categories'] = list(categoriesSet)
                 elif inBlock == 'reviews':
                     reviewsMatch = reviewsContent.match(line)
                     if not reviewsMatch:
-                        print('Does not match!')
+                        # print('Does not match!')
+                        pass
                     else:
                         date = f'{reviewsMatch.group(1)}-{reviewsMatch.group(2).rjust(2,"0")}-{reviewsMatch.group(3).rjust(2,"0")}'
                         if product.get('reviews') is None:
                             product['reviews'] = []
                         product['reviews'].append(
-                            (
-                                date,
-                                reviewsMatch.group(4),
-                                reviewsMatch.group(5),
-                                reviewsMatch.group(6),
-                                reviewsMatch.group(7)
-                            )
+                            {
+                                'date': date,
+                                'customer': reviewsMatch.group(4),
+                                'rating': reviewsMatch.group(5),
+                                'votes': reviewsMatch.group(6),
+                                'helpful': reviewsMatch.group(7)
+                            }
                         )
     return products
+
+
+def formatInsert(tableClass, args):
+    query_fields = args.keys()
+    query_values = args.values()
+    pg_fields = []
+
+    for field in query_fields:
+        pg_fields.append('%(' + field + ')s')
+
+    # print(query_values)
+    # print(pg_fields)
+    query_field_string = ', '.join(query_fields)
+    query_pg_string = ', '.join(pg_fields)
+
+    # return f'INSERT INTO {tableClass} ({query_field_string}) VALUES ({query_pg_string})'.replace('None', 'NULL')
+    return 'INSERT INTO ' + tableClass + '(' + query_field_string + ') VALUES (' + query_pg_string + ')', args
+
+
+def queryString(className):
+    p = tuple(className.__dict__.values())
+    typeList = [type(x) for x in className.__dict__.values()]
+    # print(typeList)
+    text = ''
+    table = className.__class__.__name__
+    insert_query = f"insert into {table} values("
+    for t in typeList:
+        text += '\'{}\',' if t is str else '{},'
+    text = text[:-1]
+    unformatedQuery = insert_query + text + ')'
+    formatedQuery = unformatedQuery.format(*p).replace(
+        '\'None\'', 'NULL').replace('None', 'NULL')
+
+    return formatedQuery
 
 
 def dbConnect():
@@ -165,14 +209,28 @@ def dbConnect():
     script = open('sql_create_schema.txt', 'r')
     cur.execute(script.read())
 
+    blank = ['{}', '\'{}\'']
+    dbItens = parseData()
+    for dbItem in dbItens:
+
+        product = Product(dbItem.get('ASIN'), dbItem.get(
+            'title'), dbItem.get('group'), dbItem.get('salesrank'))
+        frase = queryString(product)
+        print(frase)
+        # insert_query = """insert into Product values({},'{}','{}',{})"""
+        # format_query = insert_query.format(
+        #     product.asin, product.title, product.product_group, product.salesrank).replace(
+        #     '\'None\'', 'NULL').replace('None', 'NULL')
+
+        # cur.execute(frase[0], (frase[1]['asin'], frase[1]
+        #             ['title'], frase[1]['product_group'], frase[1]['salesrank']))
+        # cur.execute(frase)
+
     conn.commit()
     cur.close()
     conn.close()
 
 
 if __name__ == "__main__":
-    dbConnect()
 
-    dbItens = parseData()
-    for dbItem in dbItens:
-        print(dbItem)
+    dbConnect()

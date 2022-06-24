ee9d0cdc6ac945033703a22053d7971ef526d589
Igor <igor.carvalho@icomp.ufam.edu.br>
2022-06-24 02:17:33 -0400

    refactor: remove unseful command

--- scripts/download-amazon-meta.sh
+++ scripts/download-amazon-meta.sh
@@ -1,8 +1,4 @@
 #!/bin/bash 
-
-if ! [ -d /resources]; then
-    mkdir resources
-fi
 if ! [ -f /resources/amazon-meta.txt]; then
     wget https://snap.stanford.edu/data/bigdata/amazon/amazon-meta.txt.gz -O resources/amazon-meta.txt.gz
     gunzip resources/amazon-meta.txt.gz

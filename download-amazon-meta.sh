78cbfbb90a27ee52f46fbb917249164e4232687d
erlonbie <erlonminievix@gmail.com>
2022-06-24 21:37:14 -0400

    fix: fix relative path issue

--- download-amazon-meta.sh
+++ download-amazon-meta.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-if ! [ -d /resources]; then
+if ! [ -d ./resources ]; then
     mkdir resources
 fi
-if ! [ -f /resources/amazon-meta.txt]; then
+if ! [ -f ./resources/amazon-meta.txt ]; then
     wget https://snap.stanford.edu/data/bigdata/amazon/amazon-meta.txt.gz -O resources/amazon-meta.txt.gz
     gunzip resources/amazon-meta.txt.gz
 fi
